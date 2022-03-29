9220ba767f22acc887b11b2874eb343022e112e1
duriangor <949995568@qq.com>
2022-03-29 16:28:38 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 export default {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '3b78820e22084146997c2a8e70db139c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '缤果',
+  domain: 'https://www.notion.so/durians/3b78820e22084146997c2a8e70db139c',
+  author: '缤果',
 
   // open graph metadata (optional)
   description: 'Example site description',
